2fe00520a6fa7dd5d8007b265ffee7518c5ee968
Wallace Filho <wallacecmf@poli.ufrj.br>
2021-03-07 22:47:49 -0300

    add long description

--- setup.py
+++ setup.py
@@ -6,6 +6,15 @@ setup(
     packages=find_packages(include=['pythology']),
     version="1.0.0",
     description="Library dedicated to epidemiological compartmental modeling",
+    long_description="""Pythology is a scientific python package with the purpose of simplifying the implementation
+                        of epidemiological compartmental models.
+                        
+                        Using object-oriented concepts, users can build each compartment and each transfer rate between compartments
+                        in a much more high-level way than implementing the model by hand, only needing to specify each of these elements' characteristics.
+                        
+                        Thus, the package aims to eliminate the need for advanced mathematical 
+                        knowledge and allow the user to focus more on the conceptual and biological aspect with freedom
+                        to explore more model and parameter possibilities.""",
     author="Wallace Correa de Moura Filho",
     author_email='wallacecmf@poli.ufrj.br',
     license="MIT",
