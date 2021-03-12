0545c6a66dc8280e37f04ac38997695a4edba79d
Wallace Filho <wallacecmf@poli.ufrj.br>
2021-03-11 21:53:09 -0300

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name="pythology",
     packages=find_packages(include=['pythology']),
-    version="1.0.3",
+    version="1.0.4",
     description="Library dedicated to epidemiological compartmental modeling",
     long_description="""Pythology is a scientific python package with the purpose of simplifying the implementation
                         of epidemiological compartmental models.
